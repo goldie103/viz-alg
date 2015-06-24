d986dade727a4ed2195e2092c7e14a00fd6377fc
Kelly Stewart <kelly.g.stewart@education.nsw.gov.au>
2015-06-25 01:10:40 +1000

    Add general template macro for form fields
    Allows information to be input through scripts rather than html files,
    which is great for separation of concerns. Also means allowing
    programmatic changing of items more easily. Also, better for using with
    the macro. Less repetition is good.

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ from flask.ext.wtf import Form
 from wtforms import SelectField, StringField
 from wtforms.validators import ValidationError
 # local project imports
-from config import AVAILABLE_ALGS
+from config import AVAILABLE_ALGS, DEFAULT_SOURCE
 
 
 def convert_source(s):
@@ -36,5 +36,17 @@ def valid_source(form, field):
 
 class InputForm(Form):
     """ Main input form for entering desired algorithm and settings. """
-    alg = SelectField("alg", choices=AVAILABLE_ALGS)
-    source = StringField("source", [valid_source])
+
+    default_formatted = ' '.join([str(i) for i in DEFAULT_SOURCE])
+
+    alg = SelectField(
+        label="Algorithm",
+        description="A sorting algorithm you'd like to see visualized.",
+        choices=AVAILABLE_ALGS)
+
+    source = StringField(
+        label="List to sort",
+        description="A list to sort. If you don't enter anything the default"
+        " {} will be used.".format(default_formatted),
+        default=default_formatted,
+        validators=[valid_source])
