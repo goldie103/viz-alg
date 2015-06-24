292de815121bcd9320a9cb26d5ff6a7fc390c38f
Kelly Stewart <kelly.g.stewart@education.nsw.gov.au>
2015-06-24 14:45:42 +1000

    Allow script variable to pass debug parameter

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 #!flask/bin/python
+import getopt
+import sys
 from app import app
-app.run(debug=True)
+
+def main(argv):
+    """
+    Attempt to read command line options to begin server.
+    """
+    try:
+        opts, args = getopt.getopt(argv, "d", ["debug"])
+    except getopt.GetoptError:
+        print("usage: run.py [-d]")
+        sys.exit()
+
+    use_debug = False
+    for opt, arg in opts:
+        if opt == "-d":
+            use_debug = True
+
+    app.run(debug=use_debug)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
