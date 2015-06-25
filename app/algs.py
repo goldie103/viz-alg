d34e87303db302ade2b6e0ca8139ff3d3bdaffe5
Kelly Stewart <kelly.g.stewart@education.nsw.gov.au>
2015-06-25 22:09:53 +1000

    Use builtin min() function for sort_selection
    Fits better with planned sorting function as well as avoids defining own
    min() implementation.
    
    Also for fitting in better with future modifications for separate
    functions for each step.

--- app/algs.py
+++ app/algs.py
@@ -1,4 +1,34 @@
-from config import DEFAULT_SOURCE, AVAILABLE_ALGS
+# from config import DEFAULT_SOURCE, AVAILABLE_ALGS
+DEFAULT_SOURCE = [42, 0, 106, 10, 184]
+AVAILABLE_ALGS = [("sort_selection", "Selection Sort"),
+                  ("sort_bogo", "Bogosort")]
+
+
+def parse_source(source):
+    """Return source as a list of integers, using default if necessary."""
+    try:
+        if source.strip() == "":
+            return DEFAULT_SOURCE
+        else:
+            # convert to a list of integers
+            return list(map(int, source.strip().split()))
+    except AttributeError:
+        return DEFAULT_SOURCE
+
+
+def remap(l, min_val=0, max_val=200):
+    """Return a list with values mapped to a specified range."""
+    old_min = min(l)
+    for i in l:
+        try:
+            i = ((i - old_min) * (max_val - min_val) / (max(l) - old_min)
+                 + min_val)
+        except ZeroDivisionError:
+            # for case where list contains all of one value
+            # i.e. max(l) == min(l). In this case,
+            # map to half of total range.
+            i = (min_val + max_val) / 2
+    return l
 
 
 class SortAlg:
@@ -14,45 +44,19 @@ class SortAlg:
         # props for displaying in page
         self.props = {
             "name": {i[0]: i[1] for i in AVAILABLE_ALGS}[alg],
-            "source": self.parse_source(source)
+            "source": parse_source(source)
         }
         self.props["steps"] = [self.props["source"]]
         self.alg(self.props["steps"])
 
-    def parse_source(source):
-        """Return source as a list of integers, using default if necessary."""
-        try:
-            if source.strip() == "":
-                return DEFAULT_SOURCE
-            else:
-                # convert to a list of integers
-                return list(map(int, source.strip().split()))
-        except AttributeError:
-            return DEFAULT_SOURCE
-
-    def remap(l, min_val=0, max_val=200):
-        """Return a list with values mapped to a specified range."""
-        old_min = min(l)
-        for i in l:
-            try:
-                i = ((i - old_min) * (max_val - min_val) /
-                     (max(l) - old_min) + min_val)
-            except ZeroDivisionError:
-                # for case where list contains all of one value
-                # i.e. max(l) == min(l). In this case,
-                # map to half of total range.
-                i = (min_val + max_val) / 2
-        return l
-
     def sort_selection(self, steps):
         """ Build list with state of list after each stage of sort. """
         for i in range(len(steps[-1])):
-            # loop through unsorted remainder of list
-            for j in range(i, len(steps[-1])):
-                if steps[-1][i] > steps[-1][j]:
-                    steps.append(list(steps[-1]))
-                    # swap first unsorted value with smaller unsorted value
-                    steps[-1][i], steps[-1][j] = steps[-1][j], steps[-1][i]
+            cur_min = min(enumerate(steps[-1][i:]), key=lambda p: p[1])[0]
+            if steps[-1][i] != steps[-1][cur_min + i]:
+                steps.append(list(steps[-1]))
+                steps[-1][i], steps[-1][cur_min + i] \
+                    = steps[-1][cur_min + i], steps[-1][i]
 
     def sort_bogo(self, steps):
         """ Build list containing state of list after each stage of sort. """
