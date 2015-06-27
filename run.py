1db170a5e720feacc805edca8a0477cd33d1bc73
Kelly Stewart <kelly.g.stewart@education.nsw.gov.au>
2015-06-27 14:41:15 +1000

    Add internal documentation and fix docstrings

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 #!flask/bin/python
-"""
-Start webapp server on port 5000.
+"""Start a viz-alg webapp server on port 5000.
+
+usage: run.py [-d] [--debug]
+-d, --debug    Start with debug enabled, providing detailed error messages and
+               stack traces on errors. If disabled then messages will be logged
+               to tmp/viz-alg.log.
 """
 
 import sys
@@ -8,23 +12,28 @@ from app import app
 
 
 def main(argv):
-    """
-    Attempt to read command line options to begin server.
-    Only option at present is -d to enable debug mode.
+    """Read command line options and run app accordingly.
+
+    Reads arguments from argv and applies relevant options.
+    Args:
+        argv (list): Output from sys.argv excluding first item.
     """
 
     from getopt import getopt, GetoptError
 
+    # Get options and arguments from argv
     try:
         opts, args = getopt(argv, "d", ["debug"])
     except GetoptError:
         print("usage: run.py [-d] [--debug]")
         sys.exit()
 
+    # Set options
     for opt, arg in opts:
         if opt == "-d":
             debug = True
 
+    # Run the script
     try:
         app.run(debug=debug)
     except NameError:
